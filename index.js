eb0e695adc8028ada3098605a2ce8eb13f8ac933
tenor2000 <tenor2000@gmail.com>
2025-03-05 15:53:30 -0500

    some minor bug fixes and included examples

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function avgOfNums(arrayOfNumbers) {
 function getLongestString(arrayOfStrings) {
   let result = "";
   arrayOfStrings.forEach((string) => {
-    if (string.length > curr) {
+    if (string.length > result) {
       result = string;
     }
   });
@@ -49,4 +49,13 @@ function printNumToN(n, count = 1) {
   printNumToN(n, count + 1);
 }
 
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr2 = ["one", "two", "three", "four", "five"];
+
+console.log("Sum of Nums: " + sumOfNums(arr1));
+console.log("Average of Nums: " + avgOfNums(arr1));
+console.log("Longest String: " + getLongestString(arr2));
+console.log("Strings of certain length: ");
+console.log(getStrings(arr2, 3));
+console.log("Counting to N...");
 printNumToN(10);
